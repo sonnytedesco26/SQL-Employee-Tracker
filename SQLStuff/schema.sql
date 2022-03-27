b78a65232974381b5b179aa23ff9a4fe5798fd3a
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-03-26 21:26:49 -0400

    wrote up schema

---
+++ SQLStuff/schema.sql
@@ -0,0 +1,38 @@
+DROP DATABASE IF EXISTS EmployeesDB;
+GO
+CREATE DATABASE EmployeesDB;
+
+CREATE TABLE [department] (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE [role] (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    INDEX department_ind (department_id),
+    CONSTRAINT department_fk 
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    INDEX role_ind (role_id),
+    CONSTRAINT role_fk 
+    FOREIGN KEY (role_id) 
+    REFERENCES role(id)
+    ON UPDATE CASCADE ON DELETE CASCADE,
+    manager_id INT,
+    INDEX manager_ind (manager_id),
+    CONSTRAINT manager_fk
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON UPDATE CASCADE ON DELETE CASCADE
+);
